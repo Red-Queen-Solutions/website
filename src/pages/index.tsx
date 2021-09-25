0b1cf7c2c2b9ac0629d488f6c5c87919f9e41132
Pontus Lindström <pontusss85@gmail.com>
2021-09-25 08:10:09 +0200

    Add more atoms, fix landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,12 +9,14 @@ export default function Home() {
       <div className="landing-section">
         <div className="left-side">
           <h1>Skräddarsydda IT-system</h1>
-          <div>
+          <p>
             Red Queen Solutions bygger interna applikationer åt företag som inte
             nöjer sig med standardlösningar. Oavsett om du vill ersätta ett
-            befintligt system eller skapa ett helt nytt så bygger vi en lösning
-            som är perfekt för din verksamhet.
-          </div>
+            befintligt system eller skapa ett helt nytt så tar vi fram en
+            lösning perfekt anpassad efter din verksamhet. Genom erfarenhet och
+            modern teknologi jobbar vi snabbt och flexibelt för att säkerställa
+            att ert företags behov uppfylls både nu och i framtiden.
+          </p>
         </div>
         <div className="right-side">
           <img className="landing-screen-image" src={LandingScreenImage}></img>
