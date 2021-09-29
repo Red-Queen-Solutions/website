30e602785d167f84771fd84841531c6a75eb6def
Pontus Lindström <pontusss85@gmail.com>
2021-09-29 17:12:13 +0200

    Add spacing system

--- src/components/QouteSection/QouteSection.tsx
+++ src/components/QouteSection/QouteSection.tsx
@@ -6,6 +6,17 @@ export const QouteSection = () => {
     <div className="qoute-section">
       <div className="left-side">
         <h2>Mer detaljer om oss</h2>
+        <p>
+          Vi har kapacitet att leverera ert system på så lite som 4-6 veckor,
+          med hjälp av modern teknologi We are able to deliver your digital
+          product in as little as 4-6 weeks employing the lean startup approach
+          and agile software development. We are also experts at scaling
+          existing apps, helping clients to shift from inefficient solutions to
+          stable products that really grow alongside their user base. agile
+          software development. We are also experts at scaling existing apps,
+          helping clients to shift from inefficient solutions to stable products
+          that really grow alongside their user base.
+        </p>
       </div>
       <div className="right-side"></div>
     </div>
