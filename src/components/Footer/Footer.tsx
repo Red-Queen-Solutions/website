9edb817510b8e39de7fa791b2804d50d6843fb57
Pontus Lindström <pontusss85@gmail.com>
2022-01-07 09:21:42 +0100

    Add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React, { FunctionComponent } from "react";
+import "./Footer.scss";
+
+const Footer: FunctionComponent = () => {
+  return (
+    <div className="footer">
+      <span className="footer-row">Org.nr: 559205-6393</span>
+      <span className="footer-row">
+        Copyright © Red Queen Solutions 2021. Alla rättigheter förbehålls.
+      </span>
+    </div>
+  );
+};
+
+export default Footer;
