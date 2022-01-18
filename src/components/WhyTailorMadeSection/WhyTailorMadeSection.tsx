01d0257072269534cc84d041577f59d33f75e6eb
Pontus Lindström <pontusss85@gmail.com>
2022-01-18 12:30:14 +0100

    add some wtms-text

--- src/components/WhyTailorMadeSection/WhyTailorMadeSection.tsx
+++ src/components/WhyTailorMadeSection/WhyTailorMadeSection.tsx
@@ -7,25 +7,25 @@ import ContentContainer from "../ContentContainer/ContentContainer";
 export const WhyTailorMadeSection = () => {
   return (
     <ContentContainer>
-      <h3>Rubrik</h3>
-      <h2>Huvudrubrik</h2>
+      <h3>Varför skräddarsytt</h3>
+      <h2>Hur företag tjänar på egenutvecklade system</h2>
+      <p className="wtm-leftside-ingress">
+        Att köpa ett system som är specifikt utvecklat efter just er verksamhet
+        kan ha flera stora fördelar, till exempel:
+      </p>
       <div className="why-tailor-made-section">
         <div className="wtm-leftside">
-          <p className="wtm-leftside-ingress">
-            Att köpa ett skräddarsytt system har flera stora fördelar jämfört
-            med en färdig standardlösning.
-          </p>
           <Reason
-            header="Ökad produktivitet"
-            text="Lorem ipsum dolor sit amet, coneget mollis neque blandit posuere. Nulla interdum placerat nulla sed porttitor."
+            header="Framtaget specifikt för er"
+            text="Varje organisation har sitt egna sätt att jobba på. Det är svårt för företag att anpassa sina processer efter en specifik mjukvara, oavsett hur effektiv eller kraftfull mjukvaran är. Det är därför mjukvara behöver vara designat och utvecklat på ett sätt så att den kan anpassas efter affärsmodellen och följa organisationens unika processer. Skräddarsydda IT-system hjälper till att optimera era processer snarare än att ersätta dem."
           />
           <Reason
-            header="Ni sparar pengar"
-            text="Lorem ipsum mentum mauris neque, eget mollis neque blandit posuere. Nulla interdum placerat nulla sed porttitor."
+            header="Skalbarhet"
+            text="För att behålla och förbättra din konkurrenskraft över tid krävs det att du kan anpassa dig efter förändrade förutsättningar. Skräddarsydda system är enkla att förändra - nya processer och teknologier kan bli integrerade i er befintliga mjukvara när marknadstrender förändras, så att ni fortsätter vara relevanta."
           />
           <Reason
-            header="Skalbart och dynamiskt"
-            text="Lorem ipsum dolor sit amet, consectetur adium mauris neque blandit posuere. Nulla interdum placerat nulla sed porttitor."
+            header="Support"
+            text="Skräddarsydda system kommer med pålitlig teknisk support från samma utvecklare som byggde din applikation från grunden. Vi är bekanta med din mjukvara och kan hantera alla typer av problem som kan uppstå."
           />
           {/* <p className="wtm-leftside-ingress">Läs mer här.</p> */}
         </div>
