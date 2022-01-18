7c2b85774ea66090c785ff00a275b7fd0cd6c13e
Derthek <shape07_mrm@hotmail.com>
2022-01-18 19:32:31 +0100

    Small style adjustments - landing section

--- src/components/LandingSection/LandingSection.tsx
+++ src/components/LandingSection/LandingSection.tsx
@@ -17,23 +17,20 @@ export const LandingSection = () => {
       <h1 className="landing-section-header">Skräddarsydda IT-system</h1>
       <div className="landing-section">
         <div className="left-side">
-          <div className="left-side-content">
-            <p>
-              Red Queen Solutions bygger applikationer åt företag som inte nöjer
-              sig med standardlösningar. Oavsett om ni vill ersätta ett
-              befintligt system eller skapa ett helt nytt så tar vi fram en
-              lösning perfekt anpassad efter er verksamhet. <br></br>Med lång
-              erfarenhet och modern medodik jobbar vi snabbt och flexibelt för
-              att säkerställa att ert företags behov uppfylls både nu och i
-              framtiden.
-            </p>
-            <FlexContainer justifyContent="flex-end">
-              <button className="button-link-inverted" onClick={doScroll}>
-                Läs mer
-              </button>
-              <ButtonLink to="/kontakt" text="Boka ett möte" />
-            </FlexContainer>
-          </div>
+          <p>
+            Red Queen Solutions bygger applikationer åt företag som inte nöjer
+            sig med standardlösningar. Oavsett om ni vill ersätta ett befintligt
+            system eller skapa ett helt nytt så tar vi fram en lösning perfekt
+            anpassad efter er verksamhet. <br></br>Med lång erfarenhet och
+            modern medodik jobbar vi snabbt och flexibelt för att säkerställa
+            att ert företags behov uppfylls både nu och i framtiden.
+          </p>
+          <FlexContainer justifyContent="flex-end">
+            <button className="button-link-inverted" onClick={doScroll}>
+              Läs mer
+            </button>
+            <ButtonLink to="/kontakt" text="Boka ett möte" />
+          </FlexContainer>
         </div>
         <div className="right-side">
           <StaticImage
