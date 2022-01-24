162167f47361395c94c5e60f834d746fb7b7699a
Pontus Lindström <pontusss85@gmail.com>
2022-01-24 15:00:03 +0100

    responsive appsection and footer

--- src/components/ApplicationsSection/Card/Card.tsx
+++ src/components/ApplicationsSection/Card/Card.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useIsSmallScreen } from "src/hooks/useIsSmallScreen";
 import "./Card.scss";
 
 interface Props {
@@ -14,6 +15,28 @@ export const Card: React.FC<Props> = ({
   text,
   imageClassName,
 }) => {
+  const isSmallScreen = useIsSmallScreen();
+
+  if (isSmallScreen) {
+    return (
+      <div className="card">
+        <div className="card-header-container">
+          <div className="card-header-icon-container">
+            <img
+              src={icon}
+              className={`card-icon${
+                !imageClassName ? "" : ` ${imageClassName}`
+              }`}
+            />
+          </div>
+
+          <h4>{header}</h4>
+        </div>
+        <p>{text}</p>
+      </div>
+    );
+  }
+
   return (
     <div className="card">
       <img
