1eecefd0ad2e4f45143bcc163eaecb7fade048a7
Pontus Lindström <pontusss85@gmail.com>
2022-01-24 19:01:55 +0100

    working on contact form

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -7,6 +7,7 @@ import { FlexContainer } from "../FlexContainer/FlexContainer";
 import Button from "../Button/Button";
 import { useSpring, animated } from "react-spring";
 import SuccessScreen from "./SuccessScreen/SuccessScreen";
+import { useWindowSize } from "src/hooks/useWindowSize";
 
 const ContactForm: FunctionComponent = () => {
   const [state, handleSubmit] = useForm("xzbozlqd");
@@ -17,6 +18,55 @@ const ContactForm: FunctionComponent = () => {
     opacity: state.succeeded ? 0 : 1,
     config: { tension: 100, friction: 26, mass: 1 },
   });
+  const windowSize = useWindowSize();
+
+  if (state.succeeded || true) {
+    return <SuccessScreen email={"oasdad@test.se"} />;
+  }
+
+  if (windowSize.width < 769) {
+    return (
+      <form className="contact-form" onSubmit={handleSubmit}>
+        <h2 className="contact-header">Kontakta oss</h2>
+        <p className="contact-p">
+          Vi svarar gärna på dina frågor. Fyll i dina uppgifter så hör vi av oss
+          inom ett par timmar.
+        </p>
+        <div className="form-item-container">
+          <label htmlFor="name">Namn</label>
+          <input id="name" type="name" name="name" required autoFocus />
+        </div>
+
+        <div className="form-item-container">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            required
+            onChange={(e) => (emailRef.current = e.target.value)}
+          />
+        </div>
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+
+        <div className="form-item-container">
+          <label htmlFor="company">Företag</label>
+          <input id="company" type="company" name="company" required />
+        </div>
+
+        <div className="form-item-container">
+          <label htmlFor="message">Meddelande</label>
+          <textarea id="message" name="message" required rows={3} />
+        </div>
+
+        <FlexContainer justifyContent="flex-end" margin="0">
+          {state.submitting && <ClipLoader color={"#1553b7"} loading={true} />}
+
+          <Button label="SKICKA" />
+        </FlexContainer>
+      </form>
+    );
+  }
 
   return (
     <ContentContainer className="contact-form-wrapper">
