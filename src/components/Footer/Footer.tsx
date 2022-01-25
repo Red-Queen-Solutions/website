320258e1196bebfe906ec0cf278a42e0f2b8b0d2
Pontus Lindström <pontusss85@gmail.com>
2022-01-25 15:24:55 +0100

    hack icons only on desktop

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: FunctionComponent = () => {
     <div className="footer">
       <div className="footer-background" />
       <div className="footer-content">
-        <span className="footer-row">Org.nr: 559205-6393</span>
+        <span className="footer-row">Org.nr: 559&zwj;205&zwj;-&zwj;6393</span>
         <span className="footer-row">
           Copyright © Red Queen Solutions AB 2022. Alla rättigheter förbehålls.
         </span>
